5eda236247b9c06ab4652c621dfd17dfa034cdbc
kuzRK <sanalsogdanov11188@gmail.com>
2025-12-05 20:25:46 +0300

    the equal and unequal operator

--- main.cpp
+++ main.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
-
+namespace topit
+{
+  struct p_t {
+    int x, y;
+  };
+  bool operator==(p_t, p_t);
+  bool operator!=(p_t, p_t);
+  struct f_t {
+    p_t aa;
+    p_t bb;
+  };
+  struct IDraw {
+    virtual p_t begin() const = 0;
+    virtual p_t next() const = 0;
+    virtual ~IDraw() = default;
+  };
+}
 int main()
 {}
-
+bool topit::operator==(p_t a, p_t b) {
+  return a.x == b.x && a.y == b.y;
+}
+bool topit::operator!=(p_t a, p_t b) {
+  return !(a == b);
+}
